eede0ac40b26f384470b63a9deaece16e277a2c0
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-04-30 01:57:03 +0200

    Rework CI

--- docker/phpunit/phpunit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-/app/vendor/bin/phpunit /app/tests/phpunit --configuration=/app/phpunit.xml ${PHPUNIT_PARAMETERS}
--- tests/phpunit/Directory/DirectoryUtils/ChangeCurrentDirectoryMethodTest.php
+++ tests/phpunit/Directory/DirectoryUtils/ChangeCurrentDirectoryMethodTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace phpunit\Directory\DirectoryUtils;
+namespace PhpObject\Tests\PhpUnit\Directory\DirectoryUtils;
 
 use PhpObject\{
     Directory\DirectoryUtils,
