a7bb64a4c3d40e680cffbf7921eb94a4d8f3e8d3
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-05-05 23:21:19 +0200

    Fix

--- tests/phpunit/Exception/File/FileNotFoundException/CreateDefaultMessageMethodTest.php
+++ tests/phpunit/Exception/File/FileNotFoundException/CreateDefaultMessageMethodTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PhpObject\Core\Tests\PhpUnit\Exception\File\FileNotFoundException;
 
 use PhpObject\Core\{
-    Exception\Directory\DirectoryNotFoundException,
+    Exception\File\FileNotFoundException,
     Tests\PhpUnit\Exception\AbstractPhpObjectExceptionTest
 };
 
@@ -14,8 +14,8 @@ class CreateDefaultMessageMethodTest extends AbstractPhpObjectExceptionTest
     public function testCreateDefaultMessage(): void
     {
         static::assertSame(
-            DirectoryNotFoundException::createDefaultMessage('foo'),
-            'Directory "foo" not found.'
+            FileNotFoundException::createDefaultMessage('foo'),
+            'File "foo" not found.'
         );
     }
 }
