eb9e9a880770920a85c82daeaf43a8853d787647
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-06-17 19:20:18 +0200

    Add FileUtils::getInode(), getUpdatedAt() and move()

--- src/File/FileUtils.php
+++ src/File/FileUtils.php
@@ -131,6 +131,42 @@ class FileUtils
         return DateTimeImmutableUtils::createFromTimestamp($timestamp);
     }
 
+    /** @link https://www.php.net/manual/en/function.filemtime.php */
+    public static function getUpdatedAt(string $filename): \DateTimeImmutable
+    {
+        PathUtils::assertIsFile($filename);
+
+        PhpObjectErrorHandlerManager::enable();
+        $timestamp = filemtime($filename);
+        $lastError = PhpObjectErrorHandlerManager::disable(PhpObjectErrorHandlerManager::DO_NOT_ASSERT_NO_ERROR);
+        if (is_int($timestamp) === false) {
+            throw new PhpObjectException(
+                "Error while getting last modification time of file \"$filename\".",
+                $lastError
+            );
+        }
+        PhpObjectErrorHandlerManager::assertNoError();
+
+        return DateTimeImmutableUtils::createFromTimestamp($timestamp);
+    }
+
+    /** @link https://www.php.net/manual/en/function.fileinode.php */
+    public static function getInode(string $filename): int
+    {
+        PathUtils::assertIsFile($filename);
+
+        PhpObjectErrorHandlerManager::enable();
+        $return = fileinode($filename);
+        $lastError = PhpObjectErrorHandlerManager::disable(PhpObjectErrorHandlerManager::DO_NOT_ASSERT_NO_ERROR);
+        if (is_int($return) === false) {
+            throw new PhpObjectException("Error while getting inode of file \"$filename\".", $lastError);
+        }
+        PhpObjectErrorHandlerManager::assertNoError();
+
+        return $return;
+    }
+
+    /** @link https://www.php.net/manual/en/function.filectime.php */
     public static function getInodeUpdatedAt(string $filename): \DateTimeImmutable
     {
         PathUtils::assertIsFile($filename);
@@ -144,6 +180,37 @@ class FileUtils
         PhpObjectErrorHandlerManager::assertNoError();
 
         return DateTimeImmutableUtils::createFromTimestamp($timestamp);
+    }
+
+    /**
+     * @param resource|null $context
+     * @link https://www.php.net/manual/en/function.rename.php
+     */
+    public static function move(string $source, string $destination, $context = null): void
+    {
+        PathUtils::assertIsFile($source, "Source file \"$source\" not found.");
+        $destinationDirectory = DirectoryUtils::getParentDirectory($destination);
+        PathUtils::assertIsDirectory(
+            $destinationDirectory,
+            "Destination directory \"$destinationDirectory\" not found."
+        );
+
+        // PHP 7.1 and 7.2 do not allow null for $context: if no value, you should not pass this argument
+        if ($context === null) {
+            PhpObjectErrorHandlerManager::enable();
+            $result = rename($source, $destination);
+            $lastError = PhpObjectErrorHandlerManager::disable(PhpObjectErrorHandlerManager::DO_NOT_ASSERT_NO_ERROR);
+        } else {
+            PhpObjectErrorHandlerManager::enable();
+            $result = rename($source, $destination, $context);
+            $lastError = PhpObjectErrorHandlerManager::disable(PhpObjectErrorHandlerManager::DO_NOT_ASSERT_NO_ERROR);
+        }
+
+        if ($result !== true) {
+            throw new PhpObjectException("File \"$source\" cannot be moved to \"$destination\".", $lastError);
+        }
+
+        PhpObjectErrorHandlerManager::assertNoError();
     }
 
     /**
